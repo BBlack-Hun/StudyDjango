c9c07ca6ec092f1ff0ae7734ed0f8c156e24c81d
BBlack-Hun <h1103j@naver.com>
2020-10-20 18:52:56 +0900

    change code for study

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+
+    # Third party
+
+    # own
+    'products',
+    'cart',
+    'profiles',
+    'pages',
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from pages.views import home_view, contact_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', home_view, name='home'),
+    path('contact/', contact_view, name='contact')
 ]
