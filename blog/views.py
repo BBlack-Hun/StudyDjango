45b690849b1dffa6272657eb6add04490cea9b03
BBlack-Hun <h1103j@naver.com>
2020-10-21 01:47:32 +0900

    finish

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -15,10 +17,14 @@ class ArticleCreateView(CreateView):
     template_name = 'articles/article_create.html'
     form_class = ArticleModelForm
     queryset = Article.objects.all()
+    # success_url = '/'
 
     def form_valid(self, form):
         print(form.cleaned_data)
         return super().form_valid(form)
+    
+    # def get_succes_url(self):
+    #     return '/'
 
 class ArticleListView(ListView):
     template_name = 'articles/article_list.html'
@@ -31,3 +37,26 @@ class ArticleDetailView(DetailView):
     def get_object(self):
         id_ = self.kwargs.get("id")
         return get_object_or_404(Article, id=id_)
+
+class ArticleUpdateView(UpdateView):
+    template_name = 'articles/article_create.html'
+    form_class = ArticleModelForm
+    queryset = Article.objects.all()
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name = 'articles/article_delete.html'
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def get_succes_url(self):
+        return reverse('articles:article-list')
